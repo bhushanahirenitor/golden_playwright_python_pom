b9f618476dcb9eb6cb131e7232fd641d59af390d
Akshada <161299553+Akshada2906@users.noreply.github.com>
2026-09-10 21:50:03 +0530

    Add login test case for user verification

---
+++ tests/login/login_test.py
@@ -0,0 +1,6 @@
+class Test_Login:
+    def test_verify_user_login_C46(self):
+        print("Navigating to /login")
+        print("Entering username admin")
+        print("Clicking login button")
+        assert True
