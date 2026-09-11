be3c099801b3c1705c9ddc50e74e5d8c92478e50
bhushanahirenitor <bhushan.ahire@nitorinfotech.com>
2026-09-11 18:02:35 +0530

    Workflow-NF: Appended methods in tests/web/test_suite_02_web.py

---
+++ tests/web/test_suite_02_web.py
@@ -0,0 +1,10 @@
+import pytest
+from playwright.sync_api import Page
+
+
+class TestSuite02Web:
+    """Test Suite 02 for Web Platform"""
+
+    def test_suite_02_web(self, page: Page):
+        """Test case for suite 02 web"""
+        pass
