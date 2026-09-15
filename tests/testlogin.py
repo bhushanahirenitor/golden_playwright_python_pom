5101192f194a1c1585df079b3111361fa78e8677
bhushanahirenitor <bhushan.ahire@nitorinfotech.com>
2026-09-15 15:20:36 +0530

    Delete tests/testlogin.py

--- tests/testlogin.py
+++
@@ -1,6 +0,0 @@
-class Test_Login:
-    def test_verify_user_login_C46(self):
-        print("Navigating to /login")
-        print("Entering username admin")
-        print("Clicking login button")
-        assert True
